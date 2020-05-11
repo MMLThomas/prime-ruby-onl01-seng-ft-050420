be930e6e31e081257830069a3c167a702c0745bb
michael thomas <michael.thomas2014@gmail.com>
2020-05-11 07:26:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(int)
+  if int > 1
+    is_prime = true
+    int_array = (2..(int-1))
+    int_array.each do |num|
+      if int % num == 0
+        is_prime = false
+        break
+      end
+    end
+  else
+    is_prime = false
+  end
+  is_prime
+end
+      
+    
