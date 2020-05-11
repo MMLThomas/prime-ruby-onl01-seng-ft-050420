123e598219468686ba36feb80ca05b4c23400eed
michael thomas <michael.thomas2014@gmail.com>
2020-05-11 07:10:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(int)
   if int > 1
     is_prime = true
-    int_array = (2..int)
+    int_array = (2..(int-1))
     int_array.each do |num|
       if num % int == 0
         is_prime = false
