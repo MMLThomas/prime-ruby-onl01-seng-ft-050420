a37ee0ba927e387fde9965162f945a5a00d7f61d
michael thomas <michael.thomas2014@gmail.com>
2020-05-11 06:18:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,19 @@
 # Add  code here!
 def prime?(int)
   int_array = [2...int]
+  i = 0
+  is_prime = true
   if int > 1
-    int_array.detect{|i| int%i == 0}
+    while i < int_array.length
+      if int%int_array[i] == 0
+        is_prime = false
+      end
+      i += 1
+    end
   else
-    false
+    is_prime = false
   end
+  is_prime
 end
       
     
